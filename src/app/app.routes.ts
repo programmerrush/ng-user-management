4ef8d0ffa787ffd104eace389695f3188898548e
Programmer Rushikesh <37672619+programmerrush@users.noreply.github.com>
2024-04-02 11:36:50 +0530

    Design system added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+
+  {
+    path: 'profile/:id', // done
+    component: UserProfileComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full',
+  },
+];
